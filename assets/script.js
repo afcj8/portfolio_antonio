03798690f9eb63f09ecb652bc91b47e7d12097ef
afcj8 <contatoantoniojunior8@gmail.com>
2024-05-21 20:48:39 -0300

    ajustes na navbar no modo responsivo

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,19 @@
+var ul = document.querySelector('.nav-list ul');
+var menuBtn = document.querySelector('.menu-btn i');
+
 function menuShow() {
-    var menu = document.querySelector('.menu');
-    menu.classList.toggle('active');
+    if (ul.classList.contains('open')) {
+        ul.classList.remove('open');
+    } else {
+        ul.classList.add('open');
+    }
 }
+
+var links = document.querySelectorAll('.nav-list .nave');
+links.forEach(function (link) {
+    link.addEventListener('click', function () {
+        if (ul.classList.contains('open')) {
+            ul.classList.remove('open');
+        }
+    });
+});
