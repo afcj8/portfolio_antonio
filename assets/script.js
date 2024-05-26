8f8ea60d2c7a0d08256de93bdddf58a931fc9903
afcj8 <contatoantoniojunior8@gmail.com>
2024-05-26 14:51:52 -0300

    animações

--- assets/script.js
+++ assets/script.js
@@ -45,3 +45,33 @@ function loadPage() {
 }
 
 loadPage();
+
+// ANIMAÇÃO TECNOLOGIAS
+
+document.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll('.tecnologias .item');
+
+    const isInView = (el) => {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+
+    const run = () => {
+        items.forEach(item => {
+            if (isInView(item)) {
+                item.classList.add('in-view');
+            } else {
+                item.classList.remove('in-view');
+            }
+        });
+    };
+
+    window.addEventListener('load', run);
+    window.addEventListener('resize', run);
+    window.addEventListener('scroll', run);
+});
